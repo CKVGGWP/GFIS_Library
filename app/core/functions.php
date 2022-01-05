359045123dd331bc8f7d209379dc3d6b296ba41f
Christian Kyle Valdez <christiankyle.valdez@g.batstate-u.edu.ph>
2022-01-05 10:12:51 +0800

    added some functions

--- app/core/functions.php
+++ app/core/functions.php
@@ -6,3 +6,30 @@ function show($param)
     print_r($param);
     echo "</pre>";
 }
+
+function read_date($str)
+{
+    if ($str)
+        return date('F j, Y, g:i:s a', strtotime($str));
+    else
+        return null;
+}
+
+function read_time($str)
+{
+    if ($str)
+        return date('g:i A', strtotime($str));
+    else
+        return null;
+}
+
+function make_date()
+{
+    return strftime("%Y-%m-%d %H:%M:%S", time());
+}
+
+function count_id()
+{
+    static $count = 1;
+    return $count++;
+}
